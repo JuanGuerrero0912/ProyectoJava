65ca62da4f3bfd918715ae40f7efee8a974a6cb7
juanguerrero0912 <juanwarrior0912@gmail.com>
2023-09-15 01:32:35 -0500

    Se agrega lista a todos los modulos

--- target/generated-sources/annotations/Entity/HistorialMedico_.java
+++ target/generated-sources/annotations/Entity/HistorialMedico_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-14T21:01:39")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-14T22:09:16")
 @StaticMetamodel(HistorialMedico.class)
 public class HistorialMedico_ { 
 
