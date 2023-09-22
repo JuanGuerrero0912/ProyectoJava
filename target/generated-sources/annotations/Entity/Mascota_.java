b27b14e1cc287297b38e4e61862fc48eb76d63dd
juanguerrero0912 <juanwarrior0912@gmail.com>
2023-09-22 15:48:57 -0500

    Se agrega boton en permisos y se cambia la foto del inicio adoptante

--- target/generated-sources/annotations/Entity/Mascota_.java
+++ target/generated-sources/annotations/Entity/Mascota_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-22T15:28:07")
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2023-09-22T15:35:42")
 @StaticMetamodel(Mascota.class)
 public class Mascota_ { 
 
